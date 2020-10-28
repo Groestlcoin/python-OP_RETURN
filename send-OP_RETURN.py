e3c0164199ac885e07315784af60705b72bccc34
jackielove4u <jackielove4u@hotmail.com>
2020-10-28 11:37:51 +0100

    groestlize

--- send-OP_RETURN.py
+++ send-OP_RETURN.py
@@ -1,19 +1,19 @@
 # send-OP_RETURN.py
-# 
+#
 # CLI wrapper for OP_RETURN.py to send bitcoin with OP_RETURN metadata
 #
 # Copyright (c) Coin Sciences Ltd
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -33,9 +33,9 @@ if len(sys.argv)<4:
 python send-OP_RETURN.py <send-address> <send-amount> <metadata> <testnet (optional)>
 
 Examples:
-python send-OP_RETURN.py 149wHUMa41Xm2jnZtqgRx94uGbZD9kPXnS 0.001 'Hello, blockchain!'
-python send-OP_RETURN.py 149wHUMa41Xm2jnZtqgRx94uGbZD9kPXnS 0.001 48656c6c6f2c20626c6f636b636861696e21
-python send-OP_RETURN.py mzEJxCrdva57shpv62udriBBgMECmaPce4 0.001 'Hello, testnet blockchain!' 1'''
+python send-OP_RETURN.py FqLDjQPjguc5SHwM2RxMbX24fsc8WmQoBA 0.001 'Hello, blockchain!'
+python send-OP_RETURN.py FqLDjQPjguc5SHwM2RxMbX24fsc8WmQoBA 0.001 48656c6c6f2c20626c6f636b636861696e21
+python send-OP_RETURN.py muCVFTRDC6JVHuYx3qupeQ6hraoM8ENGUy 0.001 'Hello, testnet blockchain!' 1'''
 	)
 
 dummy, send_address, send_amount, metadata = sys.argv[0:4]
@@ -53,5 +53,5 @@ result=OP_RETURN_send(send_address, float(send_amount), metadata, testnet)
 if 'error' in result:
 	print('Error: '+result['error'])
 else:
-	print('TxID: '+result['txid']+'\nWait a few seconds then check on: http://'+
-		('testnet.' if testnet else '')+'coinsecrets.org/')
+	print('TxID: '+result['txid']+'\nWait a few seconds then check on: https://'+
+		('testnet.' if testnet else '')+'groestlsight.groestlcoin.org/tx/'+result['txid'])
--- store-OP_RETURN.py
+++ store-OP_RETURN.py
@@ -1,19 +1,19 @@
 # store-OP_RETURN.py
-# 
+#
 # CLI wrapper for OP_RETURN.py to store data using OP_RETURNs
 #
 # Copyright (c) Coin Sciences Ltd
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -32,7 +32,7 @@ if len(sys.argv)<2:
 '''Usage:
 python store-OP_RETURN.py <data> <testnet (optional)>'''
 	)
-	
+
 data=sys.argv[1]
 
 if len(sys.argv)>2:
@@ -43,11 +43,13 @@ else:
 data_from_hex=OP_RETURN_hex_to_bin(data)
 if data_from_hex is not None:
 	data=data_from_hex
+else:
+	data=data.decode('utf-8')
 
 result=OP_RETURN_store(data, testnet)
 
 if 'error' in result:
 	print('Error: '+result['error'])
 else:
-	print("TxIDs:\n"+"\n".join(result['txids'])+"\n\nRef: "+result['ref']+"\n\nWait a few seconds then check on: http://"+
-		('testnet.' if testnet else '')+'coinsecrets.org/')
+	print("TxIDs:\n"+"\n".join(result['txids'])+"\n\nRef: "+result['ref']+"\n\nWait a few seconds then check on: https://"+
+		('testnet.' if testnet else '')+'groestlsight.groestlcoin.org/tx/'+result['txid'])
